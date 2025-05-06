91898a7511273b1cfda47c5df34102663fad26e1
Bipana-Rai <bipanarai982@gmail.com>
2025-05-06 22:11:47 +0545

    w

--- rbackend/controller/cartItem.js
+++ rbackend/controller/cartItem.js
@@ -3,11 +3,19 @@ const cartItemSchema = require("../models/cartItemModel");
 const router = express.Router();
 router.post("/cart", async (req, res) => {
   try {
-const data=req.body
-    const newdata = new cartItemSchema(data)
+    const data = req.body;
+    const newdata = new cartItemSchema(data);
     const saveData = await newdata.save();
     console.log(data);
     res.status(201).json({ message: "cart save item", data: saveData });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.get("/getCart", async (req, res) => {
+  try {
+    const data = await cartItemSchema.find();
+    res.status(201).json({ message: "cart save item", data });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
