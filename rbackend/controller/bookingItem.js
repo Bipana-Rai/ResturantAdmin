f047a9c21fcd831b387a629cc1bf8503c8351acb
Bipana-Rai <bipanarai982@gmail.com>
2025-05-12 17:00:42 +0545

    s

--- rbackend/controller/bookingItem.js
+++ rbackend/controller/bookingItem.js
@@ -1,14 +1,22 @@
-const express=require("express")
-const router=express.Router()
-const bookingData=require("../models/bookingData")
-router.post("/addBookingDetail",async(req,res)=>{
-    const data=req.body
-    try {
-        const bookeddata=new bookingData(data)
-        const savedData=bookeddata.save()
-        res.status(200).json(savedData)
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-})
-module.exports=router
+const express = require("express");
+const router = express.Router();
+const bookingData = require("../models/bookingData");
+router.post("/addBookingDetail", async (req, res) => {
+  const data = req.body;
+  try {
+    const bookeddata = new bookingData(data);
+    const savedData = bookeddata.save();
+    res.status(200).json(savedData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+router.get("/getBookingDetail", async (req, res) => {
+  try {
+    const bookeddata =await bookingData.find();
+    res.status(200).json(bookeddata);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+module.exports = router;
