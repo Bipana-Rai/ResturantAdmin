da631b4c2e7f53aaf5afda83784e38d31b9e6022
Bipana-Rai <bipanarai982@gmail.com>
2025-05-21 14:00:39 +0545

    v

--- rbackend/controller/authentication.js
+++ rbackend/controller/authentication.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
-const secret = require("../config");
+const JWT_SECRET = require("../config");
 const signupSchema = require("../models/signupModel");
 const verifyJWT = require("../middleware/verifyJWT");
+const nodemailer = require("nodemailer");
 
 router.post("/signupData", async (req, res) => {
   try {
@@ -50,7 +51,7 @@ router.post("/loginData", async (req, res) => {
     if (!isPasswordValidate) {
       return res.status(400).json({ message: "Incorrect Password" });
     }
-    var token = jwt.sign({ ...doesExistUser.toObject() }, secret, {
+    var token = jwt.sign({ ...doesExistUser.toObject() }, JWT_SECRET, {
       expiresIn: "1h", //kati time pxi expire hune
     });
     res.cookie("jwt", token, {
@@ -74,5 +75,85 @@ router.post("/logout", (req, res) => {
     sameSite: "lax",
   });
   res.status(200).json({ message: "Logged out successfully" });
+});
+
+router.post("/auth/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const oldUser = await signupSchema.findOne({ email });
+    if (!oldUser) return res.status(404).json({ message: "User not found" });
+    const token = jwt.sign(
+      { email: oldUser.email, id: oldUser._id },
+      JWT_SECRET,
+      {
+        expiresIn: "5m",
+      }
+    );
+    const link = `http://localhost:5173/reset-password/${oldUser._id}/${token}`;
+    console.log(link);
+    // euta transporter object create garxa using smtp
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "bipanarai982@gmail.com",
+        pass: "xaarwekgibbttwbb",
+      },
+    });
+    //mail options
+    let mailOptions = {
+      from: "tastyFood@gmail.com",
+      to: oldUser.email,
+      subject: "password Reset Link",
+      html: `<p> Click <a href="${link}"> here</a> to reset your password.</p>`,
+    };
+    //send mail
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Email not sent" });
+      }
+      res.status(200).json({ message: "Reset link sent to your email" });
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+router.get("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const oldUser = await signupSchema.findOne({ _id: id });
+  if (!oldUser) {
+    return res.json({ status: "User not existed" });
+  }
+
+  try {
+    const verify = jwt.verify(token, JWT_SECRET);
+  } catch (error) {}
+  console.log(req.params);
+  res.send("done");
+});
+router.post("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+  const oldUser = await signupSchema.findOne({ _id: id });
+  if (!oldUser) {
+    return res.json({ status: "User not existed" });
+  }
+
+  try {
+    const verify = jwt.verify(token, JWT_SECRET);
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    const updateResult = await signupSchema.updateOne(
+      { _id: id },
+      { $set: { password: encryptedPassword } }
+    );
+    return res.status(200).json({ message: "Password updated successfully" });
+  } catch (error) {
+    console.error(error);
+  }
+
+  return res
+    .status(500)
+    .json({ message: "Something went wrong or token expired" });
 });
 module.exports = router;
