b35ac7a4a0ddf5b63810c1414b46a43c13d3c93a
Bipana-Rai <bipanarai982@gmail.com>
2025-05-24 19:32:38 +0545

    n

--- adminpanel/src/App.tsx
+++ adminpanel/src/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { io } from "socket.io-client";
 import "./App.css";
 import Notification from "./components/Notification";
 import SingleOrder from "./components/SingleOrder";
-import { authorizeUser, getDineIn } from "./features/items/itemSlice";
+import { getDineIn } from "./features/items/itemSlice";
 import { addNotification } from "./features/items/notificationSlice";
 import MainLayout from "./outlet/MainLayout";
 import Dashboard from "./pages/Dashboard";
@@ -13,8 +13,7 @@ import ManageBooking from "./pages/ManageBooking";
 import ManageDishes from "./pages/ManageDishes";
 import ManageTables from "./pages/ManageTables";
 import OrderLine from "./pages/OrderLine";
-import { AppDispatch, RootState } from "./store/store";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { AppDispatch } from "./store/store";
 
 const socket = io("http://localhost:5000", {
   withCredentials: true,
@@ -22,7 +21,7 @@ const socket = io("http://localhost:5000", {
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
-  const {user} =useSelector((state:RootState)=>state.item)
+  
   useEffect(() => {
     socket.on("newDineInOrder", (data) => {
       console.log("Received dine-in order via socket:", data);
@@ -34,16 +33,14 @@ function App() {
       socket.off("newDineInOrder");
     };
   }, [dispatch]);
-  useEffect(()=>{
-     dispatch(authorizeUser())
-  },)
-  console.log("use",user)
+
+ 
 
   return (
     <>
       <Routes>
         <Route element={<MainLayout />}>
-         <Route element={<ProtectedRoute />}>
+         {/* <Route element={<ProtectedRoute />}> */}
           <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/orderline" element={<OrderLine />} />
           <Route path="/managedishes/all" element={<ManageDishes />} />
@@ -52,7 +49,7 @@ function App() {
           <Route path="/managebooking" element={<ManageBooking />} />
           <Route path="/notification" element={<Notification />} />
           <Route path="/singleData/:id" element={<SingleOrder />} />
-        </Route>
+        {/* </Route> */}
          </Route>
         
       </Routes>
