e844a8e9722178aeeb4ff9268a00f4e1447242a1
Bipana-Rai <bipanarai982@gmail.com>
2025-05-24 19:32:28 +0545

    n

--- adminpanel/src/main.tsx
+++ adminpanel/src/main.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { Provider } from 'react-redux'
-import { store } from "./store/store.tsx";
+import { persistor, store } from "./store/store.tsx";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
     <Provider store={store}>
+       <PersistGate loading={"loading...."} persistor={persistor}>
       <App />
+      </PersistGate>
       </Provider>
     </BrowserRouter>
   </StrictMode>
