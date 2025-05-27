2936dd0fedade08d5cc4f21cd1c2bb9b46f45bde
Bipana-Rai <bipanarai982@gmail.com>
2025-05-27 18:06:15 +0545

    x

--- adminpanel/src/components/OrderInfoCard.tsx
+++ adminpanel/src/components/OrderInfoCard.tsx
@@ -1,12 +1,12 @@
 import { orderData } from "@/features/items/itemSlice";
 interface OrderLineProps{
   data:orderData;
-  setId:(id:string)=>void
+ 
 }
-function OrderInfoCard({data,setId}:OrderLineProps) {
+function OrderInfoCard({data,}:OrderLineProps) {
   return (
     <>
-      <div className="h-[95px] w-[250px] shrink-0 py-1 px-3 bg-[#89e7ff] rounded-xl cursor-pointer  "  onClick={() => data._id ? setId(data._id) : null}>
+      <div className="h-[95px] w-[250px] shrink-0 py-1 px-3 bg-[#89e7ff] rounded-xl cursor-pointer  " >
         <div className="flex justify-between  ">
             <p>Order#F27 </p>
             <p>Table {data.tableNumber}</p>
@@ -14,7 +14,7 @@ function OrderInfoCard({data,setId}:OrderLineProps) {
         <p>Item :8X</p>
         <div className="flex justify-between items-center   ">
             <p className="text-sm ">2 mins ago</p>
-            <div className="px-3  bg-[#066146] flex  text-gray-100 rounded-2xl text-sm py-1 "><p>in Kitchen</p></div>
+            <div className="px-3  bg-[#066146] flex  text-gray-100 rounded-2xl text-sm py-1 "><p>{data.foodStatus}</p></div>
         </div>
       </div>
 
