fd25980504ab0579173d46526355eab5a38aa923
Bipana-Rai <bipanarai982@gmail.com>
2025-05-28 17:36:11 +0545

    f

--- adminpanel/src/data/waitlist.tsx
+++ adminpanel/src/data/waitlist.tsx
@@ -1,20 +1,15 @@
 const waitlist = [
   {
     list: "All",
-    bg:"bg-gray-200"
   },
   {
     list: "in Kitchen",
-    bg:"bg-blue-500"
   },
   {
     list: "Waiting",
-    bg:"bg-red-200"
   },
   {
     list: "served",
-    bg:"bg-green-200"
   },
-
 ];
 export default waitlist;
