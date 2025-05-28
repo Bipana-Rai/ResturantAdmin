7dc8d5a486a04bac5050fec0e6b09367d1f44f70
Bipana-Rai <bipanarai982@gmail.com>
2025-05-28 18:46:20 +0545

    sa

--- adminpanel/src/pages/OrderLine.tsx
+++ adminpanel/src/pages/OrderLine.tsx
@@ -1,7 +1,7 @@
 import OrderCard from "@/components/OrderCard";
 import OrderInfoCard from "@/components/OrderInfoCard";
 import waitlist from "@/data/waitlist";
-import { getDineIn, getTakeAway} from "@/features/items/itemSlice";
+import { getDineIn, getTakeAway } from "@/features/items/itemSlice";
 import { AppDispatch, RootState } from "@/store/store";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -13,11 +13,18 @@ function OrderLine() {
   );
 
   const [showDine, setShowDine] = useState(true);
-  const [currentStatus,setCurrentStatus]=useState("All")
-  console.log(currentStatus)
- const filterDineIn=currentStatus==="All"?orderDetail:(orderDetail.filter((e)=>e.foodStatus==currentStatus))
- console.log(filterDineIn)
-
+  const [currentStatus, setCurrentStatus] = useState("All");
+  console.log(currentStatus);
+  const filterDineIn =
+    currentStatus === "All"
+      ? orderDetail
+      : orderDetail.filter((e) => e.foodStatus == currentStatus);
+  console.log(filterDineIn);
+  const filterTakeAway =
+    currentStatus === "All"
+      ? takeawayOrder
+      : takeawayOrder.filter((e) => e.foodStatus == currentStatus);
+  console.log(filterDineIn);
 
   useEffect(() => {
     dispatch(getDineIn());
@@ -29,11 +36,17 @@ function OrderLine() {
       <div className="flex fixed w-[82vw]  bg-white justify-between px-8 pt-3 border-b-1  items-center pb-2 border-gray-300 ">
         <p className="text-xl font-semibold font-serif">Order Items</p>
         <div className="flex  gap-10">
-          <button className="w-30 border-1 border-gray-400 py-1 rounded-sm text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-800" onClick={() => setShowDine(true)}>
+          <button
+            className="w-30 border-1 border-gray-400 py-1 rounded-sm text-gray-600 cursor-pointer hover:bg-gray-100 hover:text-gray-800"
+            onClick={() => setShowDine(true)}
+          >
             dine In
           </button>
-          <button className="w-30 bg-black
-           hover:bg-gray-600 py-1 rounded-sm text-gray-50 cursor-pointer" onClick={() => setShowDine(false)}>
+          <button
+            className="w-30 bg-black
+           hover:bg-gray-600 py-1 rounded-sm text-gray-50 cursor-pointer"
+            onClick={() => setShowDine(false)}
+          >
             TakeAway
           </button>
         </div>
@@ -41,21 +54,25 @@ function OrderLine() {
       <div className="rounded-xl pt-19 ps-5">
         <div className="flex justify-around px-10">
           {waitlist.map((e, i) => (
-            <button key={i} className={` w-30 cursor-pointer rounded-sm px-4 py-1 border-1  border-gray-300 `}onClick={()=>setCurrentStatus(e.list)}>
+            <button
+              key={i}
+              className={` w-30 cursor-pointer rounded-sm px-4 py-1 border-1  border-gray-300 ${currentStatus===e.list && "bg-cyan-500 text-gray-50"}`}
+              onClick={() => setCurrentStatus(e.list)}
+            >
               <p>{e.list}</p>
             </button>
           ))}
         </div>
         {showDine ? (
-          <div className="scroll-hidden py-8 flex gap-2   overflow-x-auto">
-            {filterDineIn?.map((data) => (
-              <OrderInfoCard key={data._id} data={data} />
+          <div className="scroll-hidden py-8 ps-3 flex gap-4   overflow-x-auto">
+            {filterDineIn?.map((data,i) => (
+              <OrderInfoCard key={data._id} i={i} data={data} />
             ))}
           </div>
         ) : (
           <div className="scroll-hidden py-8 flex gap-2   overflow-x-auto">
-            {takeawayOrder?.map((data) => (
-              <OrderInfoCard key={data._id} data={data} />
+            {filterTakeAway?.map((data,i) => (
+              <OrderInfoCard key={data._id} data={data} i={i}/>
             ))}
           </div>
         )}
@@ -64,13 +81,13 @@ function OrderLine() {
       {showDine ? (
         <div className="py-4 ps-7   flex gap-5 flex-wrap">
           {filterDineIn?.map((data) => (
-            <OrderCard data={data}  />
+            <OrderCard data={data} />
           ))}
         </div>
       ) : (
         <div className="py-4 ps-7   flex gap-5 flex-wrap">
-          {takeawayOrder?.map((data) => (
-            <OrderCard data={data}  />
+          {filterTakeAway?.map((data) => (
+            <OrderCard data={data} />
           ))}
         </div>
       )}
