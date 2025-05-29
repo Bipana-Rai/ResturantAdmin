56fe952f80916a66e8148d61217548df8b7663a7
Bipana-Rai <bipanarai982@gmail.com>
2025-05-29 20:02:19 +0545

    s

---
+++ adminpanel/src/data/paymethod.tsx
@@ -0,0 +1,19 @@
+const paymenthod = [
+  {
+    value:"cash",
+    bg:"bg-gray-100",
+    img:"/"
+  },
+    {
+    value:"esewa",
+    bg:"bg-green-200",
+    img:"/esewa1.png"
+  },
+    {
+    value:"khalti",
+    bg:"bg-purple-200",
+    img:"/khalti.png"
+  },
+ 
+];
+export default paymenthod;
