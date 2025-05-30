a9888809e8f25c7d68a9bd1a04437020d318c57f
Bipana-Rai <bipanarai982@gmail.com>
2025-05-30 19:34:04 +0545

    d

--- rbackend/controller/takeawayOrders.js
+++ rbackend/controller/takeawayOrders.js
@@ -130,5 +130,14 @@ router.get("/getTakeAway", async (req, res) => {
      res.status(500).json({ message: error.message });
     }
   });
+  router.delete("/deleteTakeawayReceit//:id", async (req, res) => {
+      const { id } = req.params;
+      try {
+        const deleteData = await takeawayModel.findByIdAndDelete( id );
+        res.status(200).json(deleteData);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+    });
 
 module.exports = router;
