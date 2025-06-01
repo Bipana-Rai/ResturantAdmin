85cdd14e0ce36af067db3840f24592c241b103b0
Bipana-Rai <bipanarai982@gmail.com>
2025-06-01 19:45:10 +0545

    x

--- adminpanel/src/store/store.tsx
+++ adminpanel/src/store/store.tsx
@@ -8,7 +8,7 @@ import rootReducer from "./rootReducer";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["notification"], // only persist this slice
+  whitelist: ["notification","item"], // only persist this slice
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
