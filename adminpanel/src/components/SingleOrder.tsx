b6db145fe5c942a633575d3547c79c4458487c87
Bipana-Rai <bipanarai982@gmail.com>
2025-06-15 17:21:04 +0545

    d

--- adminpanel/src/components/SingleOrder.tsx
+++ adminpanel/src/components/SingleOrder.tsx
@@ -1,6 +1,6 @@
-import { getDineIn, orderData } from "@/features/items/itemSlice";
+import { getDineIn } from "@/features/items/itemSlice";
 import { AppDispatch, RootState } from "@/store/store";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import OrderCard from "./OrderCard";
@@ -10,17 +10,13 @@ function SingleOrder() {
   const dispatch = useDispatch<AppDispatch>();
   const { orderDetail } = useSelector((state: RootState) => state.item);
   const data = orderDetail?.find((e) => e._id === id);
-
-  const [filterData, setFilterData] = useState<orderData[]>(data ? [data] : []);
   useEffect(() => {
     dispatch(getDineIn());
   }, [dispatch]);
 
   return (
     <div className="w-full h-screen flex items-center justify-center">
-       {filterData.map((order) => (
-        <OrderCard key={order._id} data={order}/>
-      ))}
+       {data && <OrderCard key={data._id} data={data} />}
     </div>
   );
 }
